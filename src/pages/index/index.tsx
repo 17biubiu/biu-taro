7a2c50c2a7c95af7ed147a95037945994204b163
17biu <nnhk001@163.com>
2020-05-09 11:13:59 +0800

    feat: 修改账户

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -28,7 +28,7 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Text>修改账户</Text>
       </View>
     )
   }
